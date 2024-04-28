b8eff3bc84436c781f1f61b34ef2eef6d0484e0d
Nikhil-Not-Nikki <nikkic2302@gmail.com>
2024-04-28 10:37:33 -0500

    heatmap working, coordinate frame is off, doesn't send OccupancyGrid message yet

--- my_package/my_package/subscriber_member_function.py
+++ my_package/my_package/subscriber_member_function.py
@@ -28,6 +28,11 @@ from geometry_msgs.msg import Vector3
 from std_msgs.msg import Header
 from std_msgs.msg import ColorRGBA
 from rclpy.clock import Clock
+from nav_msgs.msg import OccupancyGrid
+from nav_msgs.msg import MapMetaData
+
+import numpy as np
+import matplotlib.pyplot as plt
 
 class MinimalSubscriber(Node):
 
@@ -42,13 +47,25 @@ class MinimalSubscriber(Node):
 
         self.publisher = self.create_publisher(Marker, '/personal', 10)
 
+        self.second_publisher = self.create_publisher(OccupancyGrid, '/map', 1)
+
         self.reset_client = self.create_client(std_srvs.srv.Empty, 'reset')
         self.reset_req = std_srvs.srv.Empty.Request()
         self.reset_client.call_async(self.reset_req)
 
+        # grid for probability
+        self.observation_grid = [ [0]*50 for i in range(50) ]
+        self.observation_counter = 0
+        self.max_x = float('-inf')
+        self.max_y = float('-inf')
+        self.min_x = float('inf')
+        self.min_y = float('inf')
+
 
     def listener_callback(self, msg):
         ranges = msg.ranges
+
+        item_found = False
 
         for i in range(len(ranges)):
             angle = msg.angle_min + (i * msg.angle_increment)
@@ -57,6 +74,7 @@ class MinimalSubscriber(Node):
                 x = magnitude * math.cos(angle)
                 y = magnitude * math.sin(angle)
                 self.get_logger().info('I found "x: %f y: %f\n"' % (x, y))
+                item_found = True
                 
                 marker = Marker()
                 new_point = Point()
@@ -106,6 +124,88 @@ class MinimalSubscriber(Node):
 
                 #marker.points = [new_point]
                 self.publisher.publish(marker)
+
+                # set up occupancy grid message
+                occupancy_grid = OccupancyGrid()
+
+                occ_header = Header()
+                occ_header.frame_id = "/world"
+                occ_header.stamp = Clock().now().to_msg()
+                occupancy_grid.header = occ_header
+
+                occ_metadata = MapMetaData()
+                occ_metadata.map_load_time = Clock().now().to_msg()
+                occ_metadata.resolution = 1.0
+                occ_metadata.width = 50
+                occ_metadata.height = 50
+
+                occ_pose = Pose()
+                occ_quat = Quaternion()
+                occ_quat.x = 0.0
+                occ_quat.y = 0.0
+                occ_quat.z = 0.0
+                occ_quat.w = 0.0
+
+                occ_point = Point()
+                occ_point.x = 0.0
+                occ_point.y = 0.0
+                occ_point.z = 0.0
+
+                occ_pose.position = new_point
+                occ_pose.orientation = occ_quat
+
+                occ_metadata.origin = occ_pose
+
+                occupancy_grid.info = occ_metadata
+
+                # each square on the grid is 0.2 m
+                x_cell = math.floor(x / 0.2) + 25
+                y_cell = math.floor(y / 0.2) + 25
+                
+                print("X: ", x_cell, " Y: ", y_cell)
+
+                if (x_cell >= 0 and x_cell < 50) and (y_cell >= 0 and y_cell < 50):
+                    self.observation_grid[y_cell][x_cell] = self.observation_grid[y_cell][x_cell] + 1
+
+                one_dim_len = len(self.observation_grid) * len(self.observation_grid[0])
+                one_dim_arr = [0] * one_dim_len
+                for j in range(len(self.observation_grid)):
+                    for k in range(len(self.observation_grid[0])):
+                        one_dim_arr.append(self.observation_grid[j][k])
+
+                occupancy_grid.data = one_dim_arr
+                
+                self.second_publisher.publish(occupancy_grid)
+
+                if x > self.max_x:
+                    self.max_x = x
+                if y > self.max_y:
+                    self.max_y = y
+                if x < self.min_x:
+                    self.min_x = x
+                if y < self.min_y:
+                    self.min_y = y
+
+        self.observation_counter = self.observation_counter + 1
+        print("observation counter: ", self.observation_counter)
+        #print("max X = ", self.max_x)
+        #print("max Y = ", self.max_y)
+        #print("min X = ", self.min_x)
+        #print("min Y = ", self.min_y)
+
+        if self.observation_counter > 40:
+            # pixel_plot = plt.figure()
+            # pixel_plot.add_axes([0, 0, 50, 50])
+            plt.title("Map Representation")
+            plt.imshow(self.observation_grid)
+            plt.colorbar()
+            plt.show()
+
+            #for i in range(len(self.observation_grid)):
+                #for j in range(len(self.observation_grid[0])):
+                    #print(self.observation_grid[i], end=" ")
+                #print(" ")
+            
 
 
 
